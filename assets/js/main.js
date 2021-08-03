86f1396a395cdcec3e2b16fcc4025444684e0912
Bumble <bumble@nightmail.ru>
2021-08-04 01:09:29 +1100

    add Skills block, Skills animate

--- assets/js/main.js
+++ assets/js/main.js
@@ -29,7 +29,80 @@ function linkAction() {
 navLink.forEach(n => n.addEventListener('click', linkAction));
 
 /*==================== ACCORDION SKILLS ====================*/
+skillsContent = document.querySelectorAll('.skills__content');
 
+function skillsAnimation(skillsContent) {
+
+    skillsPercentage = skillsContent.querySelectorAll('.skills__percentage');
+
+    skillsPercentage.forEach(el => {
+        
+        el.style.animation = 'none';
+        el.offsetHeight;
+
+        if(el.classList.contains('skills__html')) {
+            el.style.width = "90%";
+            el.style.animation =  "progres .5s ";
+        } else if(el.classList.contains('skills__css')) {
+            el.style.width = "80%";
+            el.style.animation =  "progres .5s ";
+        } else if(el.classList.contains('skills__javascript')) {
+            el.style.width = "60%";
+            el.style.animation =  "progres .5s ";
+        } else if(el.classList.contains('skills__vue')) {
+            el.style.width = "75%";
+            el.style.animation =  "progres .5s ";
+        } else if(el.classList.contains('skills__php')) {
+            el.style.width = "80%";
+            el.style.animation =  "progres .5s ";
+        } else if(el.classList.contains('skills__mysql')) {
+            el.style.width = "85%";
+            el.style.animation =  "progres .5s ";
+        } else if(el.classList.contains('skills__laravel')) {
+            el.style.width = "75%";
+            el.style.animation =  "progres .5s ";
+        }
+
+    });
+
+}
+
+function toogleSkills(e) {
+
+    // if(e.target.classList.contains('skills__arrow')) {
+    //     e.currentTarget.classList.toggle('skills__open');
+    //     e.currentTarget.classList.toggle('skills__close');
+    // }
+
+    if(e.currentTarget.classList.toggle('skills__open')) {
+        skillsAnimation(e.currentTarget);
+    }
+    
+    e.currentTarget.classList.toggle('skills__close');
+    
+
+}
+
+skillsContent.forEach(n => n.addEventListener('click', toogleSkills));
+
+// skillsArrow = document.querySelectorAll('.skills__arrow');
+
+// function skillsAction() {
+ 
+//     const a = this.closest('.skills__open');
+//     const b = this.closest('.skills__close');
+    
+//     if(a) {
+//         a.classList.toggle('skills__close');
+//         a.classList.toggle('skills__open');
+//     } else {
+//         b.classList.toggle('skills__close');
+//         b.classList.toggle('skills__open');
+//     }
+
+// }
+
+// skillsArrow.forEach(n => n.addEventListener('click', skillsAction));
 
 /*==================== QUALIFICATION TABS ====================*/
 
